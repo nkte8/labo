ce1ca0dcfbfcb8b2068323d6fb84cca26068d406
nkte8 <nkhnd@outlook.com>
2021-10-10 19:51:30 +0900

    [change] workerのswap有効化・GPUメモリを確保する処理追加

--- scripts/k8s_reset.sh
+++ scripts/k8s_reset.sh
@@ -9,6 +9,6 @@ for n in $nodelist;do
     kubectl delete node $n
     ssh -t -o StrictHostKeyChecking=no -o ServerAliveInterval=30 $n sh -c "yes | sudo kubeadm reset;
     sudo rm -rf /var/lib/kubelet /var/lib/dockershim /var/lib/etcd /var/lib/cni /etc/kubernetes /etc/cni;
-    sudo iptables -FX;
+    sudo iptables -F;
     sudo reboot"
 done
