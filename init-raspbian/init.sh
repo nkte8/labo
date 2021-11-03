511351c3a4fc68175121a976a503e472659e37f9
nkte8 <nkhnd@outlook.com>
2021-11-03 23:03:52 +0900

    [change] init終了後にavahi-daemonを停止する設定を追加

--- init-raspbian/init.sh
+++ init-raspbian/init.sh
@@ -46,6 +46,7 @@ echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/099_${USERN
 sudo chmod 440 /etc/sudoers.d/099_${USERNAME}-nopasswd
 sudo sed -i -e 's/$/ cgroup_enable=memory cgroup_memory=1 net.ifnames=0 dwc_otg.lpm_enable=0/' /boot/cmdline.txt
 sudo passwd -d pi
+sudo systemctl disable avahi-daemon 
 sudo reboot"
 
 echo "you can access 'ssh ${USERNAME}@${W_IP}' with public-key ${W_PUB_PATH}"
